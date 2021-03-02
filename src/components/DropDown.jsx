b97fafdf66d4bff7581c79453a8f42a0f7bb0b28
Madison Fishtrom <mfishtrom@gmail.com>
2021-03-02 12:46:49 -0800

    Added Menu Dropdown

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from "react";
+import { useDetectOutsideClick } from "./useDetectOutsideClick";
+import MenuIcon from "@material-ui/icons/Menu";
+
+export default function DropDown() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => setIsActive(!isActive);
+
+  return (
+    <div className="container">
+      <div className="menu-container">
+        <button onClick={onClick} className="menu-trigger">
+          <span>
+            <MenuIcon />
+          </span>
+        </button>
+        <nav
+          ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        >
+          <ul>
+            <li>
+              <a href="#">Idle</a>
+            </li>
+            <li>
+              <a href="#">RPG</a>
+            </li>
+            <li>
+              <a href="#">Match3</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Rotate.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Rotate() {
+  return (
+    <div>
+      <div>
+        <button type="button" className="btn rotate-btn">
+          Rotate
+        </button>
+        {/* <button type="button" className="btn android-btn">
+          Android
+        </button>
+        <button type="button" className="btn ipad-btn">
+          iPad
+        </button>
+        <button type="button" className="btn iphone-btn">
+          iPhone
+        </button> */}
+      </div>
+    </div>
+  );
+}
+
+export default Rotate;
